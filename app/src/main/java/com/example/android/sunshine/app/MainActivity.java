384fe4dd07aaf4187e0edc358901069ab96ff03a
grachmannico <nicko.punya.email@gmail.com>
2015-04-11 15:36:05 +0700

    3rd commit

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -1,19 +1,9 @@
 package com.example.android.sunshine.app;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -24,7 +14,7 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, new ForecastFragment())
                     .commit();
         }
     }
@@ -55,38 +45,5 @@ public class MainActivity extends ActionBarActivity {
     /**
      * A placeholder fragment containing a simple view.
      */
-    public static class PlaceholderFragment extends Fragment {
 
-        public PlaceholderFragment() {
-        }
-
-        ArrayAdapter<String> mForecastAdapter;
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            String[] data= {
-                    "Senin 6/23 - Sunny - 31/17",
-                    "Selasa 6/24 - Rainy - 21/7",
-                    "Rabu 6/25 - Foggy - 25/11",
-                    "Kamis 6/27 - Cloudy - 31/17",
-                    "Jumat 6/28 - Rainy - 21/7",
-                    "Sabtu 6/29 - Sunny - 31/17",
-                    "Minggu 6/30 - Foggy - 25/11"
-            };
-
-            List<String> weekForecast = new ArrayList<String>(Arrays.asList(data));
-
-            mForecastAdapter = new ArrayAdapter<String>(
-                    getActivity(), //context
-                    R.layout.list_item_forecast, //id layout
-                    R.id.list_item_forecast_textview, //id text view
-                    weekForecast); //data
-
-            ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
-            listView.setAdapter(mForecastAdapter);
-            return rootView;
-        }
-    }
 }
